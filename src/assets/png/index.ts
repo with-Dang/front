97f77dad7186e7e5d436d083aadd0b9be7bf80d0
KKangHHee <skh8609@gmail.com>
2024-08-14 00:13:03 +0900

    Design: 지도 마커 이미지 추가

---
+++ src/assets/png/index.ts
@@ -0,0 +1,16 @@
+import IconCurrentLocation from "./mapCurrentLocation.png";
+import IconAccommodation from "./mapIconAccommodation.png";
+import IconBeauty from "./mapIconBeauty.png";
+import IconHospital from "./mapIconHospital.png";
+import IconPill from "./mapIconPill.png";
+import IconRestaurant from "./mapIconRestaurant.png";
+import IconShop from "./mapIconShop.png";
+
+export {
+    IconAccommodation,
+    IconBeauty, IconCurrentLocation, IconHospital,
+    IconPill,
+    IconRestaurant,
+    IconShop
+};
+
