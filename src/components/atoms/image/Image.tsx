b2b2d056ce6feb98a68db033d1197775b606753b
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-25 01:16:22 +0900

    Design: 메인 페이지 울산 여행지 Pick UI

--- src/components/atoms/image/Image.tsx
+++ src/components/atoms/image/Image.tsx
@@ -1,32 +1,22 @@
-import React from 'react';
+import React from "react";
 
 type ImageType = {
   src: string;
   width: string;
   height: string;
   borderRadius: string;
+  ratio?: boolean | number;
 };
 
-function Image({
-  src,
-  width,
-  height,
-  borderRadius
-}: ImageType) {
+function Image({ src, width, height, borderRadius, ratio = 1 }: ImageType) {
   const ImageStyle: React.CSSProperties = {
     width: width,
     height: height,
     borderRadius: borderRadius,
-    objectFit: 'cover',
-    aspectRatio: "1 / 1",
+    objectFit: "cover",
+    aspectRatio: `1 / ${ratio}`,
   };
-  return (
-    <img 
-      src={src}
-      alt='alt'
-      style={ImageStyle}
-    />
-  )
+  return <img src={src} alt="alt" style={ImageStyle} />;
 }
 
-export default Image
+export default Image;
