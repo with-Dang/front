73f27de70e289fe69d52f5b821164f3be0c5a3ab
KKangHHee <skh8609@gmail.com>
2024-09-22 00:42:06 +0900

    chore: axios, react-query 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,18 +8,34 @@ export default defineConfig({
     react(),
     svgr({
       // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
-        include: "**/*.svg",
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
     }),
   ],
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://one.marinesnow34.com",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+  //       secure: true,
+  //       ws: true,
+  //     },
+  //   },
+  // },
   resolve: {
     alias: [
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@lib', replacement: '/src/lib' },
-      { find: '@assets', replacement: '/src/assets' },
-      { find: '@styles', replacement: '/src/styles' },
-      { find: '@types', replacement: '/src/types' },
-    ]
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@lib", replacement: "/src/lib" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@types", replacement: "/src/types" },
+    ],
   },
-})
+});
