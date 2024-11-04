892b25fa53c4cc4bfe542e04166b446d51fd1672
hhbb0081 <likesun2000@naver.com>
2024-11-04 16:04:38 +0900

    Feat: 회원가입 오류 메시지 구체화

--- src/hook/signup/useSignUpMutation.ts
+++ src/hook/signup/useSignUpMutation.ts
@@ -1,33 +1,36 @@
 import { useMutation } from "@tanstack/react-query";
 import { message } from "antd";
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { useNavigate } from "react-router-dom";
 import { SignUpFormValues } from "../../components/views/signUp/SignUp";
-import { commonApis } from "../commonApi";
 
 // 회원가입 (/member/signup)
 
+const BASE_URL = import.meta.env.VITE_API_URL;
 const API_URL = "/member/signup";
+
 export function useSignUpMutation() {
   const navitgate = useNavigate();
   return useMutation<
     AxiosResponse<string>,
     Error,
-    Omit<SignUpFormValues, "id" | "passwordChk">
+    Omit<SignUpFormValues, "passwordChk">
   >({
     mutationFn: ({
       nickname,
       email,
       password,
       phone,
-    }: Omit<SignUpFormValues, "id" | "passwordChk">) => {
+    }: Omit<SignUpFormValues, "passwordChk">) => {
       const body = {
         nickname: nickname,
         email: email,
         password: password,
         phone: phone,
       };
-      return commonApis.post(API_URL, body);
+      return axios.post(`${BASE_URL}${API_URL}`, body, {
+        withCredentials: true,
+      });
     },
     onSuccess: (data) => {
       console.log(data);
@@ -36,7 +39,15 @@ export function useSignUpMutation() {
     },
     onError: (error) => {
       console.log(error);
-      message.error("이미 존재하는 회원입니다.");
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 403) {
+          message.error("이미 존재하는 회원입니다.");
+        } else if (error.response?.status === 400) {
+          message.error("비밀번호는 8자 이상이어야 합니다.");
+        } else {
+          message.error("에러가 발생했습니다. 다시 입력해주세요.");
+        }
+      }
     },
   });
 }
